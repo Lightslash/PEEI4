67b63ac1a19617287551f7a575e13b27acd632d1
stevenribette <steven.ribette@epsi.fr>
2019-04-04 15:52:55 +0200

    interface

--- interface.js
+++ interface.js
@@ -1,6 +1,7 @@
-/*
-nb coté
-nb angle droit
-nb coté même taille
-nb coté parallele
-*/
+function search(){
+    nb_cote = document.getElementById('nb_cote').value;
+    nb_angle_droit = document.getElementById('nb_angle_droit').value;
+    nb_cote_same = document.getElementById('nb_cote_same').value;
+    nb_cote_parallel = document.getElementById('nb_cote_parallel').value;
+    
+}
