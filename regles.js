a420ef7ac6a5cb94929113cb88a01fe81bfcacb2
stevenribette <steven.ribette@epsi.fr>
2019-04-17 17:50:35 +0200

    rules

--- regles.js
+++ regles.js
@@ -14,6 +14,38 @@ function read_rules(nb_cote,nb_angle_droit,nb_cote_same,nb_cote_parallel){
         }else if(nb_angle_droit==0){
             toRet = "Ceci est un triangle quelconque";
         }
+    }else if(nb_cote==4){
+        if(nb_angle_droit==4){
+            if(nb_cote_same==4){
+                toRet = "Ceci est un carré";
+            }else{
+                toRet = "Ceci est un rectangle";
+            }
+        }else if(nb_angle_droit==0){
+            if(nb_cote_parallel>2){
+                if(nb_cote_same==4){
+                    toRet = "Ceci est un losange";
+                }else{
+                    toRet = "Ceci est un parallélogramme";
+                }
+            }else if(nb_cote_parallel==2){
+                toRet = "Ceci est un trapèze";
+            }else{
+                toRet = "Ceci est un quadrilatère quelconque";
+            }
+        }
+    }else if(nb_cote==5){
+        toRet = "Ceci est un pentagone";
+    }else if(nb_cote==6){
+        toRet = "Ceci est un hexagone";
+    }else if(nb_cote==8){
+        toRet = "Ceci est un octogone";
+    }else if(nb_cote==10){
+        toRet = "Ceci est un décagone";
+    }else if(nb_cote==12){
+        toRet = "Ceci est un dodécagone";
+    }else if(nb_cote==20){
+        toRet = "Ceci est un icosagone";
     }
     return toRet;
 }
