4f9fa0811295659ce24abcd166d604e50de428de
stevenribette <steven.ribette@epsi.fr>
2019-04-18 17:38:16 +0200

    MOTEUR JSON

--- moteur_json.js
+++ moteur_json.js
@@ -20,12 +20,18 @@ function moteur(nb_cote,nb_angle_droit,nb_cote_same,nb_cote_parallel){
 function read_facts(nb_cote,nb_angle_droit,nb_cote_same,nb_cote_parallel){
     toRet = false;
     var facts = get('faits.json');
+    for(let x=0;x<facts.length;x++){
+        if(parseInt(facts[x].nb_cote)==nb_cote && parseInt(facts[x].nb_angle_droit)==nb_angle_droit && parseInt(facts[x].nb_cote_same)==nb_cote_same && parseInt(facts[x].nb_parallel)==nb_cote_parallel){
+            toRet = facts[x].ret;
+            break;
+        }
+    }
     return toRet;
 }
 function read_rules(nb_cote,nb_angle_droit,nb_cote_same,nb_cote_parallel){
     toRet = false;
     var rules = get('regles.json');
-    console.log(rules);
+    toRet = find_rules(rules,{"nb_cote":nb_cote,"nb_angle_droit":nb_angle_droit,"nb_cote_same":nb_cote_same,"nb_cote_parallel":nb_cote_parallel});
     return toRet;
 }
 function reset(){
@@ -46,6 +52,7 @@ function get(file){
     let toRet = false;
     $.ajax({type: "GET", url: url+"?file="+file, success: function(result){
         toRet = JSON.parse(result);
+        toRet = toRet.data;
     },async:false});
     return toRet;
 }
@@ -55,6 +62,23 @@ function post(file, data){
         data: {"data":JSON.stringify(data)},
         success: function(result){
             toRet = JSON.parse(result);
+            toRet = toRet.data;
     },async:false});
     return toRet;
+}
+function find_rules(arr,vars){
+    let ret = false;
+    for(let x=0;x<arr.length;x++){
+        if(vars[arr[x].var]==parseInt(arr[x].val)){
+            ret = arr[x].ret;
+            if(arr[x].more){
+                r = find_rules(arr[x].more,vars);
+                if(typeof r == 'string'){
+                    ret = r;
+                }
+            }
+            break;
+        }
+    }
+    return ret;
 }
