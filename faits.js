4cf759d00abf93806d052d1699c61060978f9471
stevenribette <steven.ribette@epsi.fr>
2019-04-18 12:08:55 +0200

    localstorage

--- faits.js
+++ faits.js
@@ -1,5 +1,10 @@
 var rules = [];
 function read_facts(nb_cote,nb_angle_droit,nb_cote_same,nb_cote_parallel){
+    if(rules.length==0){
+        if(localStorage.getItem("facts")){
+            rules = JSON.parse(localStorage.getItem("facts"));
+        }
+    }
     toRet = false;
     for(var x=0;x<rules.length;x++){
         var r = rules[x];
@@ -11,7 +16,11 @@ function read_facts(nb_cote,nb_angle_droit,nb_cote_same,nb_cote_parallel){
 }
 function add_facts(nb_cote,nb_angle_droit,nb_cote_same,nb_cote_parallel,strToRet){
     rules.push([nb_cote,nb_angle_droit,nb_cote_same,nb_cote_parallel,strToRet]);
+    localStorage.setItem("facts", JSON.stringify(rules));
 }
 function reset_facts(){
     rules = [];
+    if(localStorage.getItem("facts")){
+        localStorage.removeItem("facts");
+    }
 }
