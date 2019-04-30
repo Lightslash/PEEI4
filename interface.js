31628bbbc721350fd5756a9b92aec6515ee9bfb9
stevenribette <steven.ribette@epsi.fr>
2019-04-30 15:36:11 +0200

    interface range input

--- interface.js
+++ interface.js
@@ -3,6 +3,7 @@ function search(){
     nb_angle_droit = check_nb(document.getElementById('nb_angle_droit').value);
     nb_cote_same = check_nb(document.getElementById('nb_cote_same').value);
     nb_cote_parallel = check_nb(document.getElementById('nb_cote_parallel').value);
+    console.log(nb_cote);
     answer = moteur(nb_cote,nb_angle_droit,nb_cote_same,nb_cote_parallel);
     display_answer(answer);
 }
@@ -15,4 +16,7 @@ function check_nb(nb){
 }
 function display_answer(answer){
     document.getElementById('answer-container').innerHTML = answer;
+}
+function range(id){
+    document.getElementById(id+'_box').value = document.getElementById(id).value;
 }
